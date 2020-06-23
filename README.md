ae98d0aba56aa1b092bcbe5f80fb5afdb444c431
ssj <jooj609@gmail.com>
2020-06-23 15:44:41 +0900

    Change variable names , update README.md
    Variable name
    path ->  image_path
    file_list -> image_list
    cnt ->  image_list_index
    
    Add Variable
    change_term : You can easily modify the image change interval
    
    Delete
    else: -> It isn’t necessary
    
    README.md -> add usage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ubuntu-wallpaper-slideshow
 This changes the wallpaper with pictures inside your own directory every specified time.
+
+## usage
+1. Modify the image_path to the directory that contains the image files you want to use.
+2. If you want to change the background image fastly or slowly, modify change_term. The default is 15 minutes.
--- wallpaper_slideshow.py
+++ wallpaper_slideshow.py
@@ -2,17 +2,20 @@ import os
 import time
 import threading
 
-path = '/home/ssj/Pictures/wallpaper'
-file_list = os.listdir(path)
-cnt=0
+image_path = '/home/ssj/Pictures/wallpaper'
+image_list = os.listdir(image_path)
+change_term = 15 #minutes
+image_list_index=0
 
 def switch_wallpaper():
-	global cnt
-	os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/ssj/Pictures/wallpaper/"+file_list[cnt])
-	if cnt>=len(file_list)-1:
-		cnt = 0
-	else:
-		cnt += 1
-	threading.Timer(60*15,switch_wallpaper).start()
+	global image_list_index
+
+	os.system("gsettings set org.gnome.desktop.background picture-uri file:///home/ssj/Pictures/wallpaper/"+image_list[image_list_index])
+
+	image_list_index += 1
+	if image_list_index>=len(image_list)-1:
+		image_list_index = 0
+
+	threading.Timer(60*change_term,switch_wallpaper).start()
 
 switch_wallpaper()
